75c84f8a568e96ca612c12579c1981bcb71087bf
Antnamve <antnamve@gmail.com>
2024-06-26 13:40:40 +0300

    fix

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: process.env.CLIENT_DOMAIN,
+		// domain: process.env.CLIENT_DOMAIN,
 		sameSite: 'strict',
 		expires: 1
 	})
